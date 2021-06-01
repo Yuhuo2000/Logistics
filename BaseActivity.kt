7f4b9d2042f824b9f18a553c15fed291591b9530
LiuRJ <66198270+Yuhuo2000@users.noreply.github.com>
2021-06-01 16:58:04 +0800

    Create BaseActivity.kt

---
+++ BaseActivity.kt
@@ -0,0 +1,18 @@
+package com.example.logistics
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+open class BaseActivity: AppCompatActivity(){
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        ActivityCollector.addActivity(this)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        ActivityCollector.removeActivity(this)
+    }
+
+}
